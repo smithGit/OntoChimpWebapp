1ba2a64b66d99877ea0f8a8b517cc50281a4b08f
Sam Smith <smsmith508@aol.com>
2026-08-09 22:04:54 -0400

    Major Milestone: Azure Quasar connected with Azure FastAPI and MySQL, no LLM.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   OntoChimp Ontology Development System
 
 2026-08-04 SMS First implementation of Phase 1, Version v0.1
+2026-08-09 SMS Major Milestone: Azure Quasar connected with Azure FastAPI and MySQL, no LLM
 
 Folder: cd "D:\OntoChimpFE\OntoChimpWebapp\src"
 Execution: yarn run dev
